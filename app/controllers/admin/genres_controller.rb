d2ad1b9039d523ee36213cd84575b9f984203b56
EC2 Default User <ec2-user@ip-172-31-31-232.ap-northeast-1.compute.internal>
2022-09-09 15:10:53 +0000

    [Create]admin_genres_edit

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -4,6 +4,34 @@ class Admin::GenresController < ApplicationController
     @genres = Genre.all
   end
 
+  def create
+    @genre = Genre.new(genre_params)
+    if @genre.save
+      flash[:notice] = "ジャンルの登録が完了しました"
+      redirect_to admin_genres_path(@genre.id)
+    else
+      @genres = Genre.all
+      render :index
+    end
+  end
+
   def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+   @genre = Genre.find(params[:id])
+   if @genre.update(genre_params)
+     flash[:notice] = 'ジャンルの変更が完了しました'
+     redirect_to admin_genres_path(@genre.id)
+   else
+     render :edit
+   end
+  end
+
+  private
+
+  def genre_params
+    params.require(:genre).permit(:name)
   end
 end
