baf07ef91396bd09c78acaa18afa83829b24997d
EC2 Default User <ec2-user@ip-172-31-31-232.ap-northeast-1.compute.internal>
2022-09-10 10:19:00 +0000

    [Start]admin_items_new

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :genre, dependent: :destroy
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  enum is_active:{sale:0, discontinued:1}
+
+
+def get_image(width, height)
+  image.variant(resize_to_limit:[width,height]).processed
+end
 end
