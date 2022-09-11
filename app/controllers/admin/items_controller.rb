2284fbf899af4ad4ab1fa77cf65e607f61abf6d3
EC2 Default User <ec2-user@ip-172-31-31-232.ap-northeast-1.compute.internal>
2022-09-11 00:21:39 +0000

    [Start]admin_item_show

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,13 +3,25 @@ class Admin::ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+  end
+
   def index
     @items = Item.page(params[:page])
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
   end
+
+  private
+  def item_params
+    params.permit(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
+  end
+
+
+
 end
