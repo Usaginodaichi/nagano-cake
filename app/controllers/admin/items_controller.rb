fea71ffd8432088ac91b4af72f7e1cfd830f2b7d
EC2 Default User <ec2-user@ip-172-31-31-232.ap-northeast-1.compute.internal>
2022-09-14 03:50:16 +0000

    [Create]_item_html_erb

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -25,6 +25,12 @@ class Admin::ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    redirect_to admins_items_url
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_active)
