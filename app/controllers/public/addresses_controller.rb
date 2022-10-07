3d905a593d0270ff654c7fa386ac4bf7e26dc941
EC2 Default User <ec2-user@ip-172-31-31-232.ap-northeast-1.compute.internal>
2022-10-07 04:46:57 +0000

    [Create]public_item_controller&views_index_show

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -2,9 +2,36 @@ class Public::AddressesController < ApplicationController
   def index
     @customer = current_customer
     @address = Address.new
-    @addresses = Address.find(params[:id])
+    @addresses = Address.all
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      flash[:notice] = "住所の登録が完了しました"
+      redirect_to public_addresses_path
+    else
+      render :index
+    end
   end
 
   def edit
+    @customer = current_customer
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    if @address.update(address_params)
+      flash[:notice] = "住所の変更が完了しました"
+      redirect_to public_addresses_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -32,5 +32,6 @@ Rails.application.routes.draw do
     get 'customers/unsubscribe' =>'customers#unsubscribe'
     patch '/customers/withdraw' =>'customers#unsubscribe'
     resources:addresses, only:[:index, :edit, :create, :update, :destroy]
+    resources:items, only:[:index, :show]
   end
 end
