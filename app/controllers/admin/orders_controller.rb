79c12b3d14739d588f9f0e49d9714b58b4146d03
EC2 Default User <ec2-user@ip-172-31-31-232.ap-northeast-1.compute.internal>
2022-11-08 16:27:15 +0000

    [Update]Latout&admin_order_show

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,19 +7,14 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order_detail = @order.order_details.find(params[:id])
     @order.update(order_params)
     flash[:notice] = "注文ステータスを変更いたしました"
-    redirect_to admin_order_path
+    redirect_to admin_order_path(@order)
   end
 
 
   private
   def order_params
     params.require(:order).permit(:status)
-  end
-
-  def order_detail_params
-    params.require(:order_detail).permit(:making_status)
   end
 end
