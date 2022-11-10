e92d5f50ac1dc4e4fbebfb5c6e044c24d5761186
EC2 Default User <ec2-user@ip-172-31-31-232.ap-northeast-1.compute.internal>
2022-11-10 11:38:37 +0000

    [update]admin_order_controller

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,9 +3,14 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
       @order_detail = OrderDetail.find(params[:id])
-      @order_detail.update(order_detail_params)
+      @order = @order_detail.order
+      if @order_detail.update(order_detail_params)
+        if @order_detail.making_status = "production"
+          @order.update(status:2)
+        end
       flash[:notice] = "制作ステータスを編集いたしました"
       redirect_to admin_order_path(@order_detail.order)
+      end
   end
 
   private
--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -7,9 +7,17 @@ class Admin::OrdersController < ApplicationController
 
   def update
     @order = Order.find(params[:id])
-    @order.update(order_params)
-    flash[:notice] = "注文ステータスを変更いたしました"
-    redirect_to admin_order_path(@order)
+    @order_detail = @order.order_details.all
+    if @order.update(order_params)
+      if @order.status = "confirmation"
+       @order_detail.update_all(making_status:1)
+      end
+      if @order_detail.making_status = "completed"
+        @order.status.update(status:3)
+      end
+      flash[:notice] = "注文ステータスを変更いたしました"
+      redirect_to admin_order_path(@order)
+    end
   end
 
 
